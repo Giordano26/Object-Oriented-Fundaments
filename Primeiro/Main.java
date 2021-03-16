8e959bd926286eba054a5c8fcffaf9d9d913b804
Stefano <smfgiordano@gmail.com>
2021-03-16 15:12:02 -0300

    Names

--- Primeiro/Main.java
+++ Primeiro/Main.java
@@ -1,10 +1,10 @@
 public class Main{
 
-    static int f(int x){
-        return x+1;
+    static int f(int x){ //f --> método e x--> parâmetro
+        return x+1; //implementa a ideia de uma adição de uma unidade 
     }
 
     public static void main(String[] ps){
-        System.out.println(f(5));
+        System.out.println(f(5)); //chama o dado método que aplica a função f() que adiciona a um número uma unidade a mais
     }
 }
