b1a7ce03e70eb13b6ef6d77cd55bc7168c165021
Stefano <smfgiordano@gmail.com>
2021-03-23 12:46:29 -0300

    Improve Accuracy  and  Precision

--- IMC/imc.java
+++ IMC/imc.java
@@ -1,35 +1,49 @@
 import java.util.Scanner;
 
 public class imc{
+    
+    static double validar_a(double a){
 
+        return 
+        Math.round(100 * a)/100.0;
+    }
+    
+    
     //modelo matématico imc(p,a) == p/a²
 
-    private static double imcCalc(double p,double a){
-        double a2 = a*a;
+    private static double imcCalc(int p,double a){
 
-        return p/a2;
+        double a_sig = validar_a(a);
+        if((40 <= p) && (p <= 130) &&
+            (1.4 <= a_sig) && (a_sig <= 2.0)){
+                return p/ (a*a);
+            }
+        throw new RuntimeException("Argumentos inválidos" );
     }
+    
 
 
     public static void main(String args[]){
 
         String p,a;
-        double pD,aD;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite seu peso (em kg): ");
-        p = sc.nextLine();
-        pD = Double.parseDouble(p);
-
-        System.out.println("Digite a sua altura (em metros): ");
-        a = sc.nextLine();
-        aD = Double.parseDouble(a);
-
-        sc.close();
+        Double aD;
+        int pI;
 
 
-        String resultado = String.format("%.2f", imcCalc(pD, aD));
+            Scanner sc = new Scanner(System.in);
+    
+            System.out.println("Digite seu peso (em kg): ");
+            p = sc.nextLine();
+            pI = Integer.parseInt(p);
+    
+            System.out.println("Digite a sua altura (em metros): ");
+            a = sc.nextLine();
+            aD = Double.parseDouble(a);
+    
+            sc.close();
+
+
+        String resultado = String.format("%.2f", imcCalc(pI, aD));
 
         System.out.println("IMC = " + resultado);
 
